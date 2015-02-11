52cfcd113b2e5a7141d2256665ea2db2a1074eab
Jiri Kratochvil <kratochvil@klok.cz>
2015-02-11 15:08:26 +0100

    * prepare commandline parser

--- src/drafter.cc
+++ src/drafter.cc
@@ -1,2 +1,48 @@
-int main() {
+//
+//  snowcrash.cc
+//  snowcrash
+//
+//  Created by Jiri Kratochvil on 2015-02-11
+//  Copyright (c) 2015 Apiary Inc. All rights reserved.
+//
+//
+//
+
+#include <string>
+
+namespace config {
+    static const std::string Output         = "output";
+    static const std::string Format         = "format";
+    static const std::string Render         = "render";
+    static const std::string Sourcemap      = "sourcemap";
+    static const std::string Validate       = "validate";
+    static const std::string Version        = "version";
+    static const std::string UseLineNumbers = "use-line-num";
+};
+
+struct Config {
+};
+
+void ParseCommadLineOptions(int argc, const char *argv[], /* out */Config& config)
+{
+}
+
+//using sc = snowcrash;
+
+int main(int argc, const char *argv[])
+{
+    Config config; 
+    ParseCommadLineOptions(argc, argv, config);
+
+#if 0
+    sc::ParseResult<sc::Blueprint> blueprint;
+    sc::parse(inputStream.str(), options, blueprint);
+
+    if (!config.ValidateOnly) {
+    }
+
+    //PrintReport(blueprint.report, inputStream.str(), isUseLineNumbers);
+    return blueprint.report.error.code;
+#endif
+
 }
