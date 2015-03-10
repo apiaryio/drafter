2ae4b582db2d6b2ad4a72867b815394cc0f00899
Jiri Kratochvil <kratochvil@klok.cz>
2015-03-10 11:15:50 +0100

    * result serialization - SourceMap is now optional

--- test/test-cdrafter.cc
+++ test/test-cdrafter.cc
@@ -4,7 +4,7 @@
 
 #include <string.h>
 
-TEST_CASE("c-interface parses blueprint correctly","[c-interface]")
+TEST_CASE("c-interface parse blueprint ","[c-interface]")
 {
     ITFixtureFiles fixture = ITFixtureFiles("test/fixtures/annotations-with-warning");
 
@@ -18,6 +18,24 @@ TEST_CASE("c-interface parses blueprint correctly","[c-interface]")
 
     REQUIRE(result != NULL);
     REQUIRE(strcmp(result, fixture.get(".result.json").c_str()) == 0);
+
+    free(result);
+}
+
+TEST_CASE("c-interface parse blueprint with sourceMap","[c-interface]")
+{
+    ITFixtureFiles fixture = ITFixtureFiles("test/fixtures/annotations-with-warning");
+
+    std::string source = fixture.get(".apib");
+
+    char *result = NULL;
+
+    int ret = drafter_c_parse(source.c_str(), SC_EXPORT_SORUCEMAP_OPTION, &result);
+
+    REQUIRE(ret == 0);
+
+    REQUIRE(result != NULL);
+    REQUIRE(strcmp(result, fixture.get(".result-with-sourcemap.json").c_str()) == 0);
 
     free(result);
 }
