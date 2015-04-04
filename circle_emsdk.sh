5c134ec90fd446ff156207d7c82073561d0fee13
w-vi <vilibald@wvi.cz>
2015-04-04 21:38:35 +0200

    use chaijs for testing assertions

--- circle_emsdk.sh
+++ circle_emsdk.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
+sudo add-apt-repository -y ppa:git-core/ppa
+sudo apt-get update -y
+sudo apt-get install git
 sudo apt-get install gcc-4.7 g++-4.7
 sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.7 20
 sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-4.7 20
@@ -12,6 +15,8 @@ mkdir -p emsdk
 tar -xzf emsdk-portable.tar.gz -C emsdk --strip-components=1
 cd emsdk
 ./emsdk update
+sed -i "s|fastcomp/|fastcomp|g" emsdk_manifest.json
+sed -i "s|clang/|clang|g" emsdk_manifest.json
 ./emsdk install latest -j1
 ./emsdk activate latest
 cp ~/.emscripten .
