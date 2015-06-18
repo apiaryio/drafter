b7dcdaf44ab73a0aaa414d121c428ad2eaea7b20
Pavan Kumar Sunkara <pavan.sss1991@gmail.com>
2015-06-18 22:57:39 +0530

    Implemented rendering

--- src/RenderJSONVisitor.cc
+++ src/RenderJSONVisitor.cc
@@ -7,27 +7,114 @@
 //
 
 #include "RenderJSONVisitor.h"
+#include "sosJSON.h"
+#include "stream.h"
 
 using namespace refract;
 
 namespace drafter
 {
 
-    void RenderJSONVisitor::visit(const IElement& e) {}
+    RenderJSONVisitor::RenderJSONVisitor(const sos::Base::Type& type_)
+    : type(type_) {}
 
-    void RenderJSONVisitor::visit(const MemberElement& e) {}
+    RenderJSONVisitor::RenderJSONVisitor()
+    : type(sos::Base::UndefinedType) {}
 
-    void RenderJSONVisitor::visit(const ObjectElement& e) {}
+    void RenderJSONVisitor::assign(sos::Base value) {
+        if (type == sos::Base::ArrayType) {
+            pArr.push(value);
+        }
+        else if (type == sos::Base::UndefinedType) {
+            result = value;
+        }
+    }
+
+    void RenderJSONVisitor::assign(std::string key, sos::Base value) {
+        if (!key.empty() && type == sos::Base::ObjectType) {
+            pObj.set(key, value);
+        }
+    }
+
+    void RenderJSONVisitor::visit(const IElement& e) {
+        if (e.element() == "object") {
+            this->visit(static_cast<const ObjectElement&>(e));
+        }
+        else if (e.element() == "array") {
+            this->visit(static_cast<const ArrayElement&>(e));
+        }
+        else if (e.element() == "member") {
+            this->visit(static_cast<const MemberElement&>(e));
+        }
+        else if (e.element() == "string") {
+            this->visit(static_cast<const StringElement&>(e));
+        }
+        else if (e.element() == "number") {
+            this->visit(static_cast<const NumberElement&>(e));
+        }
+        else if (e.element() == "boolean") {
+            this->visit(static_cast<const BooleanElement&>(e));
+        }
+    }
+
+    void RenderJSONVisitor::visit(const MemberElement& e) {
+        RenderJSONVisitor renderer;
+        renderer.visit(*e.value.second);
+        assign(static_cast<const StringElement&>(*e.value.first).value, renderer.get());
+    }
+
+    void RenderJSONVisitor::visit(const ObjectElement& e) {
+        RenderJSONVisitor renderer(sos::Base::ObjectType);
+        std::vector<refract::IElement*>::const_iterator it;
+
+        for (it = e.value.begin(); it != e.value.end(); ++it) {
+            renderer.visit(*(*it));
+        }
+
+        assign(renderer.get());
+    }
+
+    void RenderJSONVisitor::visit(const ArrayElement& e) {
+        RenderJSONVisitor renderer(sos::Base::ArrayType);
+        std::vector<refract::IElement*>::const_iterator it;
+
+        for (it = e.value.begin(); it != e.value.end(); ++it) {
+            renderer.visit(*(*it));
+        }
+
+        assign(renderer.get());
+    }
 
     void RenderJSONVisitor::visit(const NullElement& e) {}
-    void RenderJSONVisitor::visit(const StringElement& e) {}
-    void RenderJSONVisitor::visit(const NumberElement& e) {}
-    void RenderJSONVisitor::visit(const BooleanElement& e) {}
-    
-    void RenderJSONVisitor::visit(const ArrayElement& e) {}
-    
-    std::string RenderJSONVisitor::get() const {
-        return "";
+
+    void RenderJSONVisitor::visit(const StringElement& e) {
+        assign(sos::String(e.value));
     }
-    
+
+    void RenderJSONVisitor::visit(const NumberElement& e) {
+        assign(sos::Number(e.value));
+    }
+
+    void RenderJSONVisitor::visit(const BooleanElement& e) {
+        assign(sos::Boolean(e.value));
+    }
+
+    sos::Base RenderJSONVisitor::get() const {
+        if (type == sos::Base::ArrayType) {
+            return pArr;
+        }
+        else if (type == sos::Base::ObjectType) {
+            return pObj;
+        }
+
+        return result;
+    }
+
+    std::string RenderJSONVisitor::getString() const {
+        sos::SerializeJSON serializer;
+        std::stringstream ss;
+
+        serializer.process(get(), ss);
+        return ss.str();
+    }
 }
--- src/RenderJSONVisitor.h
+++ src/RenderJSONVisitor.h
@@ -16,22 +16,31 @@
 namespace drafter {
 
     class RenderJSONVisitor : public refract::IVisitor {
-        sos::Object result;
+        sos::Object pObj; // partial object
+        sos::Array pArr;  // partial array
+        sos::Base result;
+
+        sos::Base::Type type;
 
     public:
-        RenderJSONVisitor() {}
+        RenderJSONVisitor(const sos::Base::Type& type);
+        RenderJSONVisitor();
+
+        void assign(sos::Base value);
+        void assign(std::string key, sos::Base value);
 
         void visit(const refract::IElement& e);
         void visit(const refract::MemberElement& e);
         void visit(const refract::ObjectElement& e);
+        void visit(const refract::ArrayElement& e);
 
         void visit(const refract::NullElement& e);
         void visit(const refract::StringElement& e);
         void visit(const refract::NumberElement& e);
         void visit(const refract::BooleanElement& e);
-        void visit(const refract::ArrayElement& e);
 
-        std::string get() const;
+        sos::Base get() const;
+        std::string getString() const;
     };
 }
 
