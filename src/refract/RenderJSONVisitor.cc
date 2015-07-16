700ab82b38e8ed2bda9d0ef619ef2fa00ee1cf72
Jiri Kratochvil <jiri@apiary.io>
2015-07-16 17:12:52 +0200

    Boutique - render JSON from "samples" and "default"

--- src/refract/RenderJSONVisitor.cc
+++ src/refract/RenderJSONVisitor.cc
@@ -65,6 +65,7 @@ namespace refract
 
     void RenderJSONVisitor::visit(const MemberElement& e) {
         RenderJSONVisitor renderer;
+
         if (e.value.second) {
             renderer.visit(*e.value.second);
         }
@@ -76,6 +77,53 @@ namespace refract
             throw std::logic_error("A property's key in the object is not of type string");
         }
     }
+
+    template<typename T>
+    const T* getDefault(const T& e) {
+        IElement::MemberElementCollection::const_iterator i = e.attributes.find("default");
+
+        if (i == e.attributes.end()) {
+            return NULL;
+        }
+
+        return TypeQueryVisitor::as<T>((*i)->value.second);
+    }
+
+    template<typename T>
+    const T* getSample(const T& e) {
+        IElement::MemberElementCollection::const_iterator i = e.attributes.find("samples");
+
+        if (i == e.attributes.end()) {
+            return NULL;
+        }
+
+        ArrayElement* a = TypeQueryVisitor::as<ArrayElement>((*i)->value.second);
+
+        if (!a || a->value.empty()) {
+            return NULL;
+        }
+
+        return TypeQueryVisitor::as<T>(*(a->value.begin()));
+    }
+
+    template<typename T, typename R = typename T::ValueType>
+    struct getValue {
+        const T& element;
+
+        getValue(const T& e) : element(e) {}
+
+        operator const R*() {
+            if (const T* d = getDefault(element)) {
+                return &d->value;
+            } 
+
+            if (const T* s = getSample(element)) {
+                return &s->value;
+            }
+
+            return &element.value;
+        }
+    };
 
     void RenderJSONVisitor::visit(const ObjectElement& e) {
         // If the element is a mixin reference
@@ -100,13 +148,21 @@ namespace refract
         }
 
         RenderJSONVisitor renderer(sos::Base::ObjectType);
-        std::vector<refract::IElement*>::const_iterator it;
+
+        const ObjectElement::ValueType* val = getValue<ObjectElement>(e);
+
+        if (!val) {
+            return;
+        }
 
         if (e.element() == "extend") {
             renderer.isExtend = true;
         }
 
-        for (it = e.value.begin(); it != e.value.end(); ++it) {
+        for (std::vector<refract::IElement*>::const_iterator it = val->begin();
+             it != val->end();
+             ++it) {
+
             if (*it) {
                 renderer.visit(*(*it));
             }
@@ -117,13 +173,21 @@ namespace refract
 
     void RenderJSONVisitor::visit(const ArrayElement& e) {
         RenderJSONVisitor renderer(sos::Base::ArrayType);
-        std::vector<refract::IElement*>::const_iterator it;
+
+        const ArrayElement::ValueType* val = getValue<ArrayElement>(e);
+
+        if (!val) {
+            return;
+        }
 
         if (e.element() == "extend") {
             renderer.isExtend = true;
         }
 
-        for (it = e.value.begin(); it != e.value.end(); ++it) {
+        for (ArrayElement::ValueType::const_iterator it = val->begin();
+             it != val->end();
+             ++it) {
+
             if (*it) {
                 renderer.visit(*(*it));
             }
@@ -135,15 +199,27 @@ namespace refract
     void RenderJSONVisitor::visit(const NullElement& e) {}
 
     void RenderJSONVisitor::visit(const StringElement& e) {
-        assign(sos::String(e.value));
+        const StringElement::ValueType* v = getValue<StringElement>(e);
+
+        if (v) {
+            assign(sos::String(*v));
+        }
     }
 
     void RenderJSONVisitor::visit(const NumberElement& e) {
-        assign(sos::Number(e.value));
+        const NumberElement::ValueType* v = getValue<NumberElement>(e);
+
+        if (v) {
+            assign(sos::Number(*v));
+        }
     }
 
     void RenderJSONVisitor::visit(const BooleanElement& e) {
-        assign(sos::Boolean(e.value));
+        const BooleanElement::ValueType* v = getValue<BooleanElement>(e);
+
+        if (v) {
+            assign(sos::Boolean(*v));
+        }
     }
 
     sos::Base RenderJSONVisitor::get() const {
