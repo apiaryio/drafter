404cab879bb5bd6775a0469f3e132ae08b3a2cf3
Daniel G. Taylor <danielgtaylor@gmail.com>
2015-10-29 09:50:28 -0700

    Fix upload asset name

--- emcc/release.sh
+++ emcc/release.sh
@@ -9,7 +9,7 @@ tar -xjf $OS-amd64-github-release.tar.bz2
 
 ./bin/$OS/amd64/github-release release -u apiaryio -r drafter --tag $TAG
 ./bin/$OS/amd64/github-release upload -u apiaryio -r drafter --tag $TAG --name drafter.js --file emcc/drafter.js
-./bin/$OS/amd64/github-release upload -u apiaryio -r drafter --tag $TAG --name drafter.js --file emcc/drafter.js.mem
+./bin/$OS/amd64/github-release upload -u apiaryio -r drafter --tag $TAG --name drafter.js.mem --file emcc/drafter.js.mem
 
 # Publish to npm
 npm --no-git-tag-version version $TAG
