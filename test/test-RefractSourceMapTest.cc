cba7d4ef127e3bbc47b843fe7787de632c7c431d
Pavan Kumar Sunkara <pavan.sss1991@gmail.com>
2015-11-06 18:15:42 +0530

    Added sourcemap tests for individual components

--- test/test-RefractSourceMapTest.cc
+++ test/test-RefractSourceMapTest.cc
@@ -3,3 +3,19 @@
 using namespace draftertest;
 
 TEST_REFRACT_SOURCE_MAP("sourcemap", "full");
+TEST_REFRACT_SOURCE_MAP("sourcemap", "description");
+TEST_REFRACT_SOURCE_MAP("sourcemap", "metadata");
+TEST_REFRACT_SOURCE_MAP("sourcemap", "resource-group");
+TEST_REFRACT_SOURCE_MAP("sourcemap", "data-structure");
+TEST_REFRACT_SOURCE_MAP("sourcemap", "resource");
+TEST_REFRACT_SOURCE_MAP("sourcemap", "action");
+TEST_REFRACT_SOURCE_MAP("sourcemap", "advanced-action");
+TEST_REFRACT_SOURCE_MAP("sourcemap", "relation");
+TEST_REFRACT_SOURCE_MAP("sourcemap", "resource-attributes");
+TEST_REFRACT_SOURCE_MAP("sourcemap", "action-attributes");
+TEST_REFRACT_SOURCE_MAP("sourcemap", "payload-attributes");
+TEST_REFRACT_SOURCE_MAP("sourcemap", "transaction");
+TEST_REFRACT_SOURCE_MAP("sourcemap", "headers");
+TEST_REFRACT_SOURCE_MAP("sourcemap", "resource-parameters");
+TEST_REFRACT_SOURCE_MAP("sourcemap", "action-parameters");
+TEST_REFRACT_SOURCE_MAP("sourcemap", "asset");
