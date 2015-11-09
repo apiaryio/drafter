99c3dd83861e36e206a4db26f3ad6d46b9baacd8
Pavan Kumar Sunkara <pavan.sss1991@gmail.com>
2015-11-10 01:48:46 +0530

    Do not use symlinks

--- test/test-RefractSourceMapTest.cc
+++ test/test-RefractSourceMapTest.cc
@@ -2,20 +2,20 @@
 
 using namespace draftertest;
 
-TEST_REFRACT_SOURCE_MAP("sourcemap", "description");
-TEST_REFRACT_SOURCE_MAP("sourcemap", "metadata");
-TEST_REFRACT_SOURCE_MAP("sourcemap", "resource-group");
-TEST_REFRACT_SOURCE_MAP("sourcemap", "data-structure");
-TEST_REFRACT_SOURCE_MAP("sourcemap", "resource");
-TEST_REFRACT_SOURCE_MAP("sourcemap", "action");
-TEST_REFRACT_SOURCE_MAP("sourcemap", "advanced-action");
-TEST_REFRACT_SOURCE_MAP("sourcemap", "relation");
-TEST_REFRACT_SOURCE_MAP("sourcemap", "resource-attributes");
-TEST_REFRACT_SOURCE_MAP("sourcemap", "action-attributes");
-TEST_REFRACT_SOURCE_MAP("sourcemap", "payload-attributes");
-TEST_REFRACT_SOURCE_MAP("sourcemap", "transaction");
-TEST_REFRACT_SOURCE_MAP("sourcemap", "headers");
-TEST_REFRACT_SOURCE_MAP("sourcemap", "resource-parameters");
-TEST_REFRACT_SOURCE_MAP("sourcemap", "action-parameters");
-TEST_REFRACT_SOURCE_MAP("sourcemap", "asset");
-TEST_REFRACT_SOURCE_MAP("sourcemap", "mson");
+TEST_REFRACT_SOURCE_MAP("api", "description");
+TEST_REFRACT_SOURCE_MAP("api", "metadata");
+TEST_REFRACT_SOURCE_MAP("api", "resource-group");
+TEST_REFRACT_SOURCE_MAP("api", "data-structure");
+TEST_REFRACT_SOURCE_MAP("api", "resource");
+TEST_REFRACT_SOURCE_MAP("api", "action");
+TEST_REFRACT_SOURCE_MAP("api", "advanced-action");
+TEST_REFRACT_SOURCE_MAP("api", "relation");
+TEST_REFRACT_SOURCE_MAP("api", "resource-attributes");
+TEST_REFRACT_SOURCE_MAP("api", "action-attributes");
+TEST_REFRACT_SOURCE_MAP("api", "payload-attributes");
+TEST_REFRACT_SOURCE_MAP("api", "transaction");
+TEST_REFRACT_SOURCE_MAP("api", "headers");
+TEST_REFRACT_SOURCE_MAP("api", "resource-parameters");
+TEST_REFRACT_SOURCE_MAP("api", "action-parameters");
+TEST_REFRACT_SOURCE_MAP("api", "asset");
+TEST_REFRACT_SOURCE_MAP("api", "mson");
