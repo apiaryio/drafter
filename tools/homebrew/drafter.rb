b3cacde11713c58b8ebc2c27b680c90810e5a366
Kyle Fuller <kyle@fuller.li>
2015-11-26 13:30:09 +0000

    Release 2.0.0

--- tools/homebrew/drafter.rb
+++ tools/homebrew/drafter.rb
@@ -2,7 +2,7 @@ require 'formula'
 
 class Drafter < Formula
   homepage 'http://apiblueprint.org'
-  head 'https://github.com/apiaryio/drafter.git', :tag => 'v1.0.0'
+  head 'https://github.com/apiaryio/drafter.git', :tag => 'v2.0.0'
 
   def install
     system "./configure"
