d90d4d1ad1978c113ceb116f765cd67cbe2a9a70
Pavan Kumar Sunkara <pavan.sss1991@gmail.com>
2016-04-15 14:51:38 +0200

    Release 2.3.0

--- tools/homebrew/drafter.rb
+++ tools/homebrew/drafter.rb
@@ -2,7 +2,7 @@ require 'formula'
 
 class Drafter < Formula
   homepage 'http://apiblueprint.org'
-  head 'https://github.com/apiaryio/drafter.git', :tag => 'v2.3.0-pre.2'
+  head 'https://github.com/apiaryio/drafter.git', :tag => 'v2.3.0'
 
   def install
     system "./configure"
