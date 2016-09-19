ccbdddb537510a957e4d7eaacde0a0c687395924
Jiri Kratochvil <jiri@apiary.io>
2016-09-19 12:06:37 +0200

    bugfix: warn on action with non-absolut URI
    Fix#350

---
+++ test/test-SyntaxIssuesTest.cc
@@ -0,0 +1,14 @@
+//
+//  test-SyntaxIssuesTest.cc
+//  drafter
+//
+//  Created by Jiří Kratochvíl on 19-09-2016.
+//  Copyright (c) 2016 Apiary. All rights reserved.
+//
+
+#include "draftertest.h"
+
+using namespace draftertest;
+
+TEST_REFRACT("syntax", "issue-350");
+
