eb41c1e0a723d97a6c0b110912f209f9c5fc9a19
e.baranov <e.baranov@fun-box.ru>
2017-02-09 11:33:32 +0300

    add one more test

--- test/test-OneOfTest.cc
+++ test/test-OneOfTest.cc
@@ -8,6 +8,7 @@ TEST_REFRACT("oneof", "object");
 TEST_REFRACT("oneof", "multi-properties");
 TEST_REFRACT("oneof", "expanded");
 TEST_REFRACT("oneof", "required");
+TEST_REFRACT("oneof", "required2");
 
 
 // FIXME: invalid JSON Schema
