987cd647f47735685cb439915d9843c818a01ba1
e.baranov <e.baranov@fun-box.ru>
2017-04-30 18:18:59 +0300

    add common data tests

---
+++ test/test-CommonDataTest.cc
@@ -0,0 +1,8 @@
+
+#include "draftertest.h"
+
+using namespace draftertest;
+
+TEST_REFRACT("common-data", "simple");
+TEST_REFRACT("common-data", "nested");
+TEST_REFRACT("common-data", "multiple-groups");
