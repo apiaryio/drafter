7aa1d35bc3f5e0a987c1c8af1bac8a2dbe618741
Jiri Kratochvil <jiri@apiary.io>
2018-03-26 09:47:01 +0200

    Change comparator algorithm

--- src/ElementComparator.h
+++ src/ElementComparator.h
@@ -14,53 +14,43 @@
 
 namespace drafter
 {
+
+    namespace detail
+    {
+
+        struct SortedRef {
+            using InfoRef = std::vector<std::reference_wrapper<const refract::InfoElements::value_type> >;
+
+            InfoRef operator()(const refract::InfoElements& infoElements, const std::set<std::string>& ignore) const
+            {
+                InfoRef res;
+
+                std::transform(infoElements.begin(), infoElements.end(), std::back_inserter(res), [](auto& info) {
+                    return std::cref(info);
+                });
+
+                res.erase(std::remove_if(res.begin(),
+                              res.end(),
+                              [&ignore](const auto& info) { return ignore.find(info.get().first) != ignore.end(); }),
+                    res.end());
+
+                std::sort(res.begin(), res.end(), [](auto l, auto r) { return l.get().first < r.get().first; });
+
+                return res;
+            }
+        };
+    };
+
     using namespace refract;
     struct InfoElementsComparator {
-        bool operator()(const InfoElements& rhs, const InfoElements& lhs)
+        bool operator()(const InfoElements& rhs, const InfoElements& lhs) const
         {
+            const auto l = detail::SortedRef()(lhs, { "sourceMap" });
+            const auto r = detail::SortedRef()(rhs, { "sourceMap" });
 
-            const std::set<std::string> ignore = { "sourceMap" };
-            std::set<std::string> keys;
-
-            if (std::find_if(lhs.begin(),
-                    lhs.end(),
-                    [&keys, &rhs, &ignore](const auto& info) {
-                        auto key = info.first;
-                        if (ignore.find(key) != ignore.end()) {
-                            return false;
-                        }
-                        keys.insert(key);
-
-                        auto rInfo = rhs.find(key);
-                        if (rInfo == rhs.end()) {
-                            return true;
-                        }
-
-                        return !(*rInfo->second == *info.second);
-                    })
-                != lhs.end()) {
-                return false;
-            }
-
-            if (std::find_if(rhs.begin(),
-                    rhs.end(),
-                    [&keys, &lhs, &ignore](const auto& info) {
-                        auto key = info.first;
-                        if (ignore.find(key) != ignore.end()) {
-                            return false;
-                        }
-
-                        if (keys.find(key) != ignore.end()) {
-                            return false;
-                        }
-
-                        return true;
-                    })
-                != rhs.end()) {
-                return false;
-            }
-
-            return true;
+            return std::equal(l.begin(), l.end(), r.begin(), r.end(), [](const auto& l, const auto& r) {
+                return l.get().first == r.get().first && *l.get().second.get() == *r.get().second.get();
+            });
         }
     };
 
@@ -68,15 +58,12 @@ namespace drafter
         const IElement& rhs;
 
         template <typename ElementT>
-        bool operator()(const ElementT& lhs)
+        bool operator()(const ElementT& lhs) const
         {
-            if (auto rhsptr = dynamic_cast<const ElementT*>(&rhs)) {
-                return (lhs.empty() == rhs.empty()) && (lhs.element() == rhs.element())
-                    && (InfoElementsComparator{}(rhs.attributes(), lhs.attributes()))
-                    && (InfoElementsComparator{}(rhs.meta(), lhs.meta()))
-                    && (lhs.empty() || (lhs.get() == rhsptr->get()));
-            } else
-                return false;
+            return (lhs.empty() == rhs.empty()) && (lhs.element() == rhs.element())
+                && (InfoElementsComparator{}(rhs.attributes(), lhs.attributes()))
+                && (InfoElementsComparator{}(rhs.meta(), lhs.meta()))
+                && (lhs.empty() || (lhs.get() == dynamic_cast<const ElementT*>(&rhs)->get()));
         }
     };
 } // namespace drafter
