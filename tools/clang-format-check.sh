73abac1bb80a448c210b000143ca290758b2faba
Kyle Fuller <kyle@fuller.li>
2018-06-19 12:36:15 +0100

    chore(ci): Use clang-format validation on CI

--- tools/clang-format-check.sh
+++ tools/clang-format-check.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-clang-format-5.0 -style=file -output-replacements-xml $(git ls-files | grep '\.cc\|\.h$') | grep -c "<replacement "
+clang-format -style=file -output-replacements-xml $(git ls-files | grep '\.cc\|\.h$') | grep -c "<replacement "
+
 if [ $? -ne 1 ]
 then
 	echo "There are some style issues. Please run clang-format on your files."
