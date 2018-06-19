7c96ef0da0d1121b2405abc0ea81ae8671720b0d
Kyle Fuller <kyle@fuller.li>
2018-06-19 16:51:43 +0100

    refactor: Break release script into functions

--- tools/release.sh
+++ tools/release.sh
@@ -1,17 +1,27 @@
 #!/bin/sh
 
-OS=`uname -s | tr '[:upper:]' '[:lower:]'`
 TAG=`git describe --tags $CIRCLE_SHA1`
-
-# Download GitHub releases script
-curl -L -O https://github.com/aktau/github-release/releases/download/v0.6.2/$OS-amd64-github-release.tar.bz2
-tar -xjf $OS-amd64-github-release.tar.bz2
-GITHUB_RELEASE=./bin/$OS/amd64/github-release
-
-# Create GitHub release
-$GITHUB_RELEASE release -u apiaryio -r drafter --tag $TAG
-
-# Create and upload tarball
 TARBALL=drafter-$TAG.tar.gz
-./tools/make-tarball.sh $TAG
-$GITHUB_RELEASE upload -u apiaryio -r drafter --tag $TAG --name $TARBALL --file $TARBALL
+
+download_gh_release() {
+  # Download GitHub releases script
+  OS=`uname -s | tr '[:upper:]' '[:lower:]'`
+  curl -L -O https://github.com/aktau/github-release/releases/download/v0.6.2/$OS-amd64-github-release.tar.bz2
+  tar -xjf $OS-amd64-github-release.tar.bz2
+  export PATH="./bin/$OS/amd64/:$PATH"
+}
+
+create_release() {
+  # Create GitHub release
+  github-release release -u apiaryio -r drafter --tag $TAG
+  github-release upload -u apiaryio -r drafter --tag $TAG --name $TARBALL --file $TARBALL
+}
+
+create_tarball() {
+  # Create release tarball
+  ./tools/make-tarball.sh $TAG
+}
+
+download_gh_release
+create_tarball
+create_release
