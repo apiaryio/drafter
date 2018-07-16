d00afa9bb3b23438d219f3c2319f2e671118a768
Thomas Jandecka <thomas.jandecka@oracle.com>
2018-07-16 12:47:35 +0200

    (impl) APIE -> value and APIE -> SO renderers

---
+++ src/refract/JsonValue.cc
@@ -0,0 +1,442 @@
+//
+//  refract/JsonValue.cc
+//  librefract
+//
+//  Created by Thomas Jandecka on 12/07/2018
+//  Copyright (c) 2018 Apiary Inc. All rights reserved.
+//
+
+#include "JsonValue.h"
+
+#include "../ElementData.h"
+#include "../utils/log/Trivial.h"
+#include "../utils/so/JsonIo.h"
+#include "Element.h"
+#include "ElementUtils.h"
+#include "Utils.h"
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <regex>
+
+using namespace refract;
+using namespace drafter::utils;
+using namespace drafter::utils::log;
+
+namespace
+{
+    using TypeAttributes = std::bitset<4>;
+    constexpr std::size_t FIXED_FLAG = 0;
+    constexpr std::size_t FIXED_TYPE_FLAG = 1;
+    constexpr std::size_t NULLABLE_FLAG = 2;
+    constexpr std::size_t REQUIRED_FLAG = 3;
+
+    TypeAttributes updateTypeAttributes(const IElement& e, TypeAttributes options) noexcept
+    {
+        if (hasFixedTypeAttr(e))
+            options.set(FIXED_FLAG);
+
+        if (hasFixedTypeTypeAttr(e))
+            options.set(FIXED_TYPE_FLAG);
+
+        if (hasNullableTypeAttr(e))
+            options.set(NULLABLE_FLAG);
+
+        if (hasRequiredTypeAttr(e))
+            options.set(REQUIRED_FLAG);
+
+        return options;
+    }
+
+    TypeAttributes pass_flags(TypeAttributes options) noexcept
+    {
+        return options;
+    }
+
+    TypeAttributes inherit_flags(TypeAttributes options) noexcept
+    {
+        options.reset(FIXED_TYPE_FLAG);
+        options.reset(NULLABLE_FLAG);
+        options.reset(REQUIRED_FLAG);
+        return options;
+    }
+
+    TypeAttributes inherit_or_pass_flags(TypeAttributes options, const IElement& e)
+    {
+        auto result = inherit_flags(options);
+        if (inheritsFixed(e)) {
+            LOG(debug) << "\"" << e.element() << "\"-Element inherits fixed";
+            return result;
+        }
+        return result.reset(FIXED_FLAG);
+    }
+} // namespace
+
+namespace
+{
+    void renderProperty(so::Object& obj, const MemberElement& e, TypeAttributes options);
+    void renderProperty(so::Object& obj, const RefElement& e, TypeAttributes options);
+    void renderProperty(so::Object& obj, const SelectElement& e, TypeAttributes options);
+    void renderProperty(so::Object& obj, const ObjectElement& e, TypeAttributes options);
+    void renderProperty(so::Object& obj, const ExtendElement& e, TypeAttributes options);
+    void renderProperty(so::Object& obj, const IElement& e, TypeAttributes options);
+
+    void renderItem(so::Array& obj, const RefElement& e, TypeAttributes options);
+    void renderItem(so::Array& obj, const IElement& e, TypeAttributes options);
+
+    so::Value renderValue(const ObjectElement& e, TypeAttributes options);
+    so::Array renderValue(const ArrayElement& e, TypeAttributes options);
+    so::Value renderValue(const EnumElement& e, TypeAttributes options);
+    so::Value renderValue(const NullElement& e, TypeAttributes options);
+    so::Value renderValue(const StringElement& e, TypeAttributes options);
+    so::Value renderValue(const NumberElement& e, TypeAttributes options);
+    so::Value renderValue(const BooleanElement& e, TypeAttributes options);
+    so::Value renderValue(const ExtendElement& e, TypeAttributes options);
+    so::Value renderValue(const IElement& e, TypeAttributes options);
+
+    template <typename T>
+    void renderProperty(so::Object&, const T& e, TypeAttributes)
+    {
+        LOG(error) << "invalid property element: " << e.element();
+        assert(false);
+    }
+
+    template <typename T>
+    void renderItem(so::Array& a, const T& e, TypeAttributes options)
+    {
+        LOG(debug) << "rendering item " << e.element() << "Element as JSON Value";
+        a.data.emplace_back(renderValue(e, inherit_or_pass_flags(options, e)));
+    }
+
+    template <typename T>
+    so::Value renderValue(const T& e, TypeAttributes)
+    {
+        LOG(error) << "invalid top level element: " << e.element();
+        assert(false);
+        return so::Null{}; // unreachable
+    }
+
+    so::Value renderValue(const IElement& e, TypeAttributes options)
+    {
+        return refract::visit(e, [options](const auto& el) { //
+            return renderValue(el, options);
+        });
+    }
+} // namespace
+
+so::Value refract::generateJsonValue(const IElement& el)
+{
+    return renderValue(el, TypeAttributes{});
+}
+
+namespace
+{
+
+    so::Value renderValue(const ObjectElement& e, TypeAttributes options)
+    {
+        LOG(debug) << "rendering ObjectElement to JSON Value";
+
+        options = updateTypeAttributes(e, options);
+
+        so::Object result{};
+        if (!e.empty())
+            for (const auto& item : e.get()) {
+                assert(item);
+                if (options.test(FIXED_TYPE_FLAG) || options.test(FIXED_FLAG))
+                    renderProperty(
+                        result, *item, inherit_or_pass_flags(options, *item) | TypeAttributes{}.set(REQUIRED_FLAG));
+                else
+                    renderProperty(result, *item, inherit_or_pass_flags(options, *item));
+            }
+
+        // TODO handle nullable?
+
+        return result;
+    }
+
+    const IElement& resolve(const RefElement& e)
+    {
+        const auto& resolvedEntry = e.attributes().find("resolved");
+        if (resolvedEntry == e.attributes().end()) {
+            LOG(error) << "expected all references to be resolved in backend";
+            assert(false);
+        }
+
+        assert(resolvedEntry->second);
+        return *resolvedEntry->second;
+    }
+
+    so::Array renderValue(const ArrayElement& e, TypeAttributes options)
+    {
+        LOG(debug) << "rendering ArrayElement to JSON Value";
+
+        options = updateTypeAttributes(e, options);
+
+        so::Array result{};
+        if (!e.empty())
+            for (const auto& entry : e.get()) {
+                assert(entry);
+                renderItem(result, *entry, inherit_or_pass_flags(options, *entry));
+                if (options.test(FIXED_TYPE_FLAG))
+                    break;
+            }
+
+        return result;
+    }
+
+    so::Value renderValue(const EnumElement& e, TypeAttributes options)
+    {
+        LOG(debug) << "rendering EnumElement to JSON Value";
+
+        options = updateTypeAttributes(e, options);
+
+        if (e.empty()) {
+            auto enumerationsIt = e.attributes().find("enumerations");
+            if (e.attributes().end() != enumerationsIt) {
+                const auto enums = get<const ArrayElement>(enumerationsIt->second.get());
+                assert(enums);
+                if (!enums->empty())
+                    for (const auto& enumEntry : enums->get()) {
+                        assert(enumEntry);
+                        return renderValue(*enumEntry, inherit_flags(options));
+                    }
+            }
+
+            if (options.test(NULLABLE_FLAG)) {
+                return so::Null{};
+            }
+
+            LOG(warning) << "no value found for EnumElement; using `null`";
+            return so::Null{};
+        }
+
+        assert(e.get().value());
+        return renderValue(*e.get().value(), inherit_flags(options));
+    } // namespace
+
+    so::Value renderValue(const NullElement& e, TypeAttributes options)
+    {
+        LOG(debug) << "rendering NullElement to JSON Value";
+
+        return so::Null{};
+    }
+
+    so::Value renderValue(const StringElement& e, TypeAttributes options)
+    {
+        LOG(debug) << "rendering StringElement to JSON Value";
+
+        options = updateTypeAttributes(e, options);
+
+        if (e.empty()) {
+            if (options.test(NULLABLE_FLAG))
+                return so::Null{};
+
+            if (const auto& sampleValue = findSample(e)) {
+                return renderValue(*sampleValue, options);
+            }
+
+            if (const auto& defaultValue = findDefault(e)) {
+                return renderValue(*defaultValue, options);
+            }
+
+            LOG(warning) << "no value for non-nullable empty StringElement; using `\"\"`";
+            return so::String{};
+        }
+
+        return so::String{ e.get().get() };
+    }
+
+    so::Value renderValue(const NumberElement& e, TypeAttributes options)
+    {
+        LOG(debug) << "rendering NumberElement to JSON Value";
+
+        options = updateTypeAttributes(e, options);
+
+        if (e.empty()) {
+            if (options.test(NULLABLE_FLAG))
+                return so::Null{};
+
+            if (const auto& sampleValue = findSample(e)) {
+                return renderValue(*sampleValue, options);
+            }
+
+            if (const auto& defaultValue = findDefault(e)) {
+                return renderValue(*defaultValue, options);
+            }
+
+            LOG(warning) << "no value for non-nullable empty NumberElement; using `0`";
+            return so::Number{};
+        }
+
+        return so::Number{ e.get().get() };
+    }
+
+    so::Value renderValue(const BooleanElement& e, TypeAttributes options)
+    {
+        LOG(debug) << "rendering BooleanElement to JSON Value";
+
+        options = updateTypeAttributes(e, options);
+
+        if (e.empty()) {
+            if (options.test(NULLABLE_FLAG))
+                return so::Null{};
+
+            if (const auto& sampleValue = findSample(e)) {
+                return renderValue(*sampleValue, options);
+            }
+
+            if (const auto& defaultValue = findDefault(e)) {
+                return renderValue(*defaultValue, options);
+            }
+
+            LOG(warning) << "no value for non-nullable empty NumberElement; using `false`";
+            return so::False{};
+        }
+
+        if (e.get().get())
+            return so::True{};
+        return so::False{};
+    }
+
+    so::Value renderValue(const ExtendElement& e, TypeAttributes options)
+    {
+        LOG(debug) << "rendering ExtendElement to JSON Value";
+
+        auto merged = e.get().merge();
+        assert(merged);
+        return renderValue(*merged, options);
+    }
+
+} // namespace
+
+namespace
+{
+
+    void renderProperty(so::Object& obj, const MemberElement& e, TypeAttributes options)
+    {
+        LOG(debug) << "rendering property MemberElement as JSON Value";
+
+        if (hasFixedTypeAttr(e))
+            options.set(FIXED_FLAG);
+
+        options.set(FIXED_TYPE_FLAG, hasFixedTypeTypeAttr(e));
+        options.set(NULLABLE_FLAG, hasNullableTypeAttr(e));
+
+        if (hasRequiredTypeAttr(e))
+            options.set(REQUIRED_FLAG);
+
+        if (hasOptionalTypeAttr(e))
+            options.reset(REQUIRED_FLAG);
+
+        const auto k = e.get().key();
+        const auto v = e.get().value();
+
+        assert(k);
+        // if (hasOptionalTypeAttr(e))
+        //    LOG(debug) << "omitting optional property while rendering value";
+        // else
+        if (isVariable(e)) {
+            LOG(warning) << "omitting variable property while rendering value";
+        } else {
+            auto strKey = key(e);
+            emplace_unique(obj, strKey, renderValue(*v, pass_flags(options)));
+        }
+    }
+    void renderProperty(so::Object& obj, const RefElement& e, TypeAttributes options)
+    {
+        LOG(debug) << "rendering property RefElement as JSON Value";
+
+        const auto& resolvedEntry = e.attributes().find("resolved");
+        if (resolvedEntry == e.attributes().end()) {
+            LOG(error) << "expected all references to be resolved in backend";
+            assert(false);
+        }
+
+        assert(resolvedEntry->second);
+        renderProperty(obj, *resolvedEntry->second, pass_flags(options));
+    }
+
+    void renderProperty(so::Object& s, const SelectElement& e, TypeAttributes options)
+    {
+        LOG(debug) << "rendering property SelectElement as JSON Value";
+
+        so::Array oneOfs{};
+        for (const auto& option : e.get()) {
+
+            if (option->empty()) {
+                LOG(error) << "unexpected empty option element in backend";
+                assert(false);
+            }
+
+            if (option->get().size() < 1) {
+                LOG(warning) << "empty option element in backend; skipping it";
+                continue;
+            }
+
+            for (const auto& optionEntry : option->get()) {
+                assert(optionEntry);
+                renderProperty(s, *optionEntry, pass_flags(options));
+            }
+
+            return;
+        }
+
+        LOG(warning) << "no non-empty OptionElement in SelectElement; skipping property";
+    }
+
+    void renderProperty(so::Object& s, const ObjectElement& e, TypeAttributes options)
+    {
+        LOG(debug) << "rendering property ObjectElement as JSON Value";
+
+        if (hasFixedTypeAttr(e))
+            options.set(FIXED_FLAG);
+
+        if (!e.empty())
+            for (const auto& item : e.get()) {
+                assert(item);
+                renderProperty(s, *item, inherit_flags(options));
+            }
+    }
+
+    void renderProperty(so::Object& s, const ExtendElement& e, TypeAttributes options)
+    {
+        LOG(debug) << "rendering property ExtendElement as JSON Value";
+
+        if (e.empty())
+            LOG(warning) << "empty data structure element in backend";
+
+        auto merged = e.get().merge();
+        assert(merged);
+        renderProperty(s, *merged, pass_flags(options));
+    }
+
+    void renderProperty(so::Object& s, const IElement& e, TypeAttributes options)
+    {
+        auto objPtr = &s;
+        refract::visit(e, [objPtr, options](const auto& el) { //
+            renderProperty(*objPtr, el, options);
+        });
+    }
+
+    void renderItem(so::Array& a, const RefElement& e, TypeAttributes options)
+    {
+        LOG(debug) << "rendering item RefElement as JSON Value";
+
+        const auto& resolved = resolve(e);
+        if (const auto& mixin = get<const ArrayElement>(&resolved)) {
+            if (!mixin->empty())
+                for (const auto& item : mixin->get()) {
+                    assert(item);
+                    renderItem(a, *item, inherit_or_pass_flags(options, *item));
+                }
+        }
+    }
+
+    void renderItem(so::Array& a, const IElement& e, TypeAttributes options)
+    {
+        auto aPtr = &a;
+        refract::visit(e, [aPtr, options](const auto& el) { //
+            renderItem(*aPtr, el, options);
+        });
+    }
+} // namespace
---
+++ src/refract/SerializeSo.cc
@@ -0,0 +1,197 @@
+//
+//  refract/SerializeSo.cc
+//  librefract
+//
+//  Created by Thomas Jandecka on 11/07/2018
+//  Copyright (c) 2018 Apiary Inc. All rights reserved.
+//
+
+#include "SerializeSo.h"
+
+#include "../utils/log/Trivial.h"
+#include "Element.h"
+
+using namespace refract;
+using namespace serialize;
+using namespace drafter::utils;
+using namespace drafter::utils::log;
+
+namespace
+{
+    so::Object serialize(const InfoElements& info, bool renderSourceMaps);
+
+    so::Array serializeContent(const dsd::Object& e, bool renderSourceMaps);
+    so::Array serializeContent(const dsd::Array& e, bool renderSourceMaps);
+    so::Value serializeContent(const dsd::Enum& e, bool renderSourceMaps);
+    so::Null serializeContent(const dsd::Null& e, bool renderSourceMaps);
+    so::String serializeContent(const dsd::String& e, bool renderSourceMaps);
+    so::Number serializeContent(const dsd::Number& e, bool renderSourceMaps);
+    so::Value serializeContent(const dsd::Boolean& e, bool renderSourceMaps);
+    so::Array serializeContent(const dsd::Extend& e, bool renderSourceMaps);
+    so::Array serializeContent(const dsd::Select& e, bool renderSourceMaps);
+    so::Array serializeContent(const dsd::Option& e, bool renderSourceMaps);
+    so::Value serializeContent(const dsd::Holder& e, bool renderSourceMaps);
+    so::Object serializeContent(const dsd::Member& e, bool renderSourceMaps);
+    so::String serializeContent(const dsd::Ref& e, bool renderSourceMaps);
+
+    so::Value serializeAny(const IElement& e, bool renderSourceMaps)
+    {
+        so::Object result;
+
+        LOG(debug) << "Serializing element `" << e.element() << "`";
+        result.data.emplace_back("element", so::String{ e.element() });
+
+        {
+            LOG(debug) << "Serializing meta of absolute length " << e.meta().size();
+            auto meta = serialize(e.meta(), renderSourceMaps);
+            if (!meta.data.empty())
+                result.data.emplace_back("meta", std::move(meta));
+
+            LOG(debug) << "Serializing meta of absolute length " << e.meta().size() << " [DONE]";
+        }
+
+        {
+            LOG(debug) << "Serializing attribute of absolute length " << e.attributes().size();
+            auto attr = serialize(e.attributes(), renderSourceMaps || e.element() == "annotation");
+            if (!attr.data.empty())
+                result.data.emplace_back("attributes", std::move(attr));
+
+            LOG(debug) << "Serializing attribute of absolute length " << e.attributes().size() << " [DONE]";
+        }
+
+        if (!e.empty()) {
+            result.data.emplace_back("content", visit(e, [renderSourceMaps](const auto& el) { //
+                return serializeContent(el.get(), renderSourceMaps);
+            }));
+        }
+
+        return result;
+    }
+} // namespace
+
+namespace
+{
+    so::Object serialize(const InfoElements& info, bool renderSourceMaps)
+    {
+        so::Object result;
+        for (const auto& entry : info) {
+            assert(entry.second);
+            if (renderSourceMaps || entry.first != "sourceMap")
+                result.data.emplace_back(entry.first, serializeAny(*entry.second, renderSourceMaps));
+        }
+        return result;
+    }
+
+    template <typename ValueT>
+    so::Array serializeListContent(const ValueT& e, bool renderSourceMaps)
+    {
+        so::Array result;
+
+        for (const auto& entry : e) {
+            assert(entry);
+            result.data.emplace_back(serializeAny(*entry, renderSourceMaps));
+        }
+
+        return result;
+    }
+
+} // namespace
+
+namespace
+{
+    so::Array serializeContent(const dsd::Object& value, bool renderSourceMaps)
+    {
+        LOG(debug) << "Serializing ObjectElement content";
+        return serializeListContent(value, renderSourceMaps);
+    }
+
+    so::Array serializeContent(const dsd::Array& value, bool renderSourceMaps)
+    {
+        LOG(debug) << "Serializing ArrayElement content";
+        return serializeListContent(value, renderSourceMaps);
+    }
+
+    so::Value serializeContent(const dsd::Enum& value, bool renderSourceMaps)
+    {
+        LOG(debug) << "Serializing EnumElement content";
+        assert(value.value());
+        return serializeAny(*value.value(), renderSourceMaps);
+    }
+
+    so::Null serializeContent(const dsd::Null& value, bool)
+    {
+        LOG(debug) << "Serializing NullElement content";
+        return so::Null{};
+    }
+
+    so::String serializeContent(const dsd::String& value, bool)
+    {
+        LOG(debug) << "Serializing StringElement content";
+        return so::String{ value.get() };
+    }
+
+    so::Number serializeContent(const dsd::Number& value, bool)
+    {
+        LOG(debug) << "Serializing NumberElement content";
+        return so::Number{ value.get() };
+    }
+
+    so::Value serializeContent(const dsd::Boolean& value, bool)
+    {
+        LOG(debug) << "Serializing BooleanElement content";
+        if (value.get())
+            return so::True{};
+        return so::False{};
+    }
+
+    so::Array serializeContent(const dsd::Extend& value, bool renderSourceMaps)
+    {
+        LOG(debug) << "Serializing ExtendElement content";
+        return serializeListContent(value, renderSourceMaps);
+    }
+
+    so::Array serializeContent(const dsd::Select& value, bool renderSourceMaps)
+    {
+        LOG(debug) << "Serializing SelectElement content";
+        return serializeListContent(value, renderSourceMaps);
+    }
+
+    so::Array serializeContent(const dsd::Option& value, bool renderSourceMaps)
+    {
+        LOG(debug) << "Serializing OptionElement content";
+        return serializeListContent(value, renderSourceMaps);
+    }
+
+    so::Value serializeContent(const dsd::Holder& value, bool renderSourceMaps)
+    {
+        LOG(debug) << "Serializing HolderElement content";
+        assert(value.data());
+        return serializeAny(*value.data(), renderSourceMaps);
+    }
+
+    so::Object serializeContent(const dsd::Member& value, bool renderSourceMaps)
+    {
+        LOG(debug) << "Serializing MemberElement content";
+        so::Object result;
+
+        assert(value.key());
+        result.data.emplace_back("key", serializeAny(*value.key(), renderSourceMaps));
+
+        if (const auto v = value.value())
+            result.data.emplace_back("value", serializeAny(*value.value(), renderSourceMaps));
+        return result;
+    }
+
+    so::String serializeContent(const dsd::Ref& value, bool)
+    {
+        LOG(debug) << "Serializing RefElement content";
+        return so::String{ value.symbol() };
+    }
+
+} // namespace
+
+so::Value serialize::renderSo(const IElement& el, bool sourceMaps)
+{
+    LOG(info) << "Starting API Elements -> SO serialization";
+    return serializeAny(el, sourceMaps);
+}
