043bc5ffed143a7ef284002be76766512777be4c
Kyle Fuller <kyle@fuller.li>
2018-07-18 13:31:58 +0100

    fix(refract-filter): Restore script to work without pyyaml

--- tools/refract-filter.py
+++ tools/refract-filter.py
@@ -14,18 +14,19 @@ except ImportError:
 VERSION = "0.1"
 
 
-def yaml_load(stream, Loader=yaml.Loader, object_pairs_hook=OrderedDict):
-    class OrderedLoader(Loader):
-        pass
+if yaml:
+    def yaml_load(stream, Loader=yaml.Loader, object_pairs_hook=OrderedDict):
+        class OrderedLoader(Loader):
+            pass
 
-    def construct_mapping(loader, node):
-        loader.flatten_mapping(node)
-        return object_pairs_hook(loader.construct_pairs(node))
+        def construct_mapping(loader, node):
+            loader.flatten_mapping(node)
+            return object_pairs_hook(loader.construct_pairs(node))
 
-    OrderedLoader.add_constructor(
-        yaml.resolver.BaseResolver.DEFAULT_MAPPING_TAG,
-        construct_mapping)
-    return yaml.load(stream, OrderedLoader)
+        OrderedLoader.add_constructor(
+            yaml.resolver.BaseResolver.DEFAULT_MAPPING_TAG,
+            construct_mapping)
+        return yaml.load(stream, OrderedLoader)
 
 
 def walk(node, cb):
