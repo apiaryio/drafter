60550cff4f18a64731b9388e57423f203bb27981
Thomas Jandecka <thomas.jandecka@oracle.com>
2019-04-25 11:56:21 +0200

    ci: use CTest for builds in CircleCI

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,7 +1,5 @@
 require 'aruba/cucumber'
 
 Before do
-  @dirs << "../../features/fixtures"
-  
-  ENV['PATH'] = "#{ENV['PWD']}/src#{File::PATH_SEPARATOR}#{ENV['PATH']}"  
+  @dirs << "features/fixtures"
 end
--- integration.cmake
+++ integration.cmake
@@ -1,7 +1,6 @@
 cmake_minimum_required(VERSION 3.6 FATAL_ERROR)
 
-find_program(Bundler bundle)
+find_program(Cucumber cucumber)
+add_test(NAME DrafterIntegration COMMAND ${Cucumber} .)
+set_property(TEST DrafterIntegration PROPERTY ENVIRONMENT "PATH=${Drafter_BUILD_DIR}")
 
-file(COPY ${CMAKE_CURRENT_LIST_DIR}/features DESTINATION ${CMAKE_CURRENT_BINARY_DIR})
-add_custom_target(BundleInstall ALL COMMAND ${Bundler} install --path vendor/bundle)
-add_test(NAME DrafterIntegration COMMAND ${Bundler} exec cucumber)
