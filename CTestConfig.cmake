01e1e2d7af920500c5b432f6b67607656d52cfb8
Thomas Jandecka <thomas.jandecka@oracle.com>
2019-05-07 14:30:38 +0200

    ctest: remove guiding comment

--- CTestConfig.cmake
+++ CTestConfig.cmake
@@ -2,9 +2,6 @@
 ## Then modify the CMakeLists.txt file in the root directory of your
 ## project to incorporate the testing dashboard.
 ##
-## # The following are required to submit to the CDash dashboard:
-##   ENABLE_TESTING()
-##   INCLUDE(CTest)
 
 set(CTEST_PROJECT_NAME "Drafter")
 set(CTEST_NIGHTLY_START_TIME "01:00:00 UTC")
