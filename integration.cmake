1d0454631f85c91582046736c16745d871fc7284
Thomas Jandecka <thomas.jandecka@oracle.com>
2019-05-07 14:31:31 +0200

    ctest: set PATH correctly when running integration tests

--- integration.cmake
+++ integration.cmake
@@ -2,5 +2,5 @@ cmake_minimum_required(VERSION 3.6 FATAL_ERROR)
 
 find_program(Cucumber cucumber)
 add_test(NAME DrafterIntegration COMMAND ${Cucumber} .)
-set_property(TEST DrafterIntegration PROPERTY ENVIRONMENT "PATH=${Drafter_BUILD_DIR}")
+set_property(TEST DrafterIntegration PROPERTY ENVIRONMENT "PATH=$ENV{PATH};${Drafter_BUILD_DIR}")
 
