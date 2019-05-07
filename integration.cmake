fc5e2798dcb3d7d4f5352f80d88ee57cc106ddae
Thomas Jandecka <thomas.jandecka@oracle.com>
2019-05-07 17:45:22 +0200

    cmake/tests: use `bundle exec` to run cucumber

--- integration.cmake
+++ integration.cmake
@@ -1,6 +1,6 @@
 cmake_minimum_required(VERSION 3.6 FATAL_ERROR)
 
-find_program(Cucumber cucumber)
-add_test(NAME DrafterIntegration COMMAND ${Cucumber} .)
+find_program(Bundle bundle)
+add_test(NAME DrafterIntegration COMMAND ${Bundle} exec cucumber .)
 set_property(TEST DrafterIntegration PROPERTY ENVIRONMENT "PATH=$ENV{PATH};${Drafter_BUILD_DIR}")
 
